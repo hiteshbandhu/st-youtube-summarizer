01c68af9a430e2f179fbabd90a4fff74b893d772
Hitesh Bandhu <90546635+hiteshbandhu@users.noreply.github.com>
2023-04-18 00:47:48 +0530

    TOML Errors on Streamlab, so hard-coded

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit as st
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import TextFormatter
 
+OPENAIKEY = "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"
+
 openai.api_key = OPENAIKEY
 
 def BasicGeneration(userPrompt):
